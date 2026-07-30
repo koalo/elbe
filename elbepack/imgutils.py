1d1c117482108791f6e6ca880f1ac214e029371b
Florian Kauer <florian.kauer@linutronix.de>
2026-07-30 10:02:43 +0200

    elbepack: imgutils: do not depend on udev if it does not exist
    Inside a container, we do not have udev running. Therefore, there is no
    need to wait for potential interference with udev.
    
    Also setup UUID as needed e.g. for grub, because that is usually also
    done by udev.
    
    Signed-off-by: Florian Kauer <florian.kauer@linutronix.de>

--- elbepack/imgutils.py
+++ elbepack/imgutils.py
@@ -4,10 +4,46 @@
 
 import contextlib
 import fcntl
+import os
 import pathlib
+import stat
 import subprocess
 
 from elbepack.shellhelper import ELBE_LOGGING, do, run
+
+_UDEV_WAIT_TIMEOUT = 30
+
+
+def _udev_available():
+    return pathlib.Path('/run/udev/control').is_socket()
+
+
+def _mknod_from_sysfs(device_name):
+    devpath = f'/dev/{device_name}'
+    if os.path.exists(devpath):
+        return
+
+    dev_attr = pathlib.Path('/sys/class/block', device_name, 'dev').read_text().strip()
+    major, minor = (int(x) for x in dev_attr.split(':'))
+    with contextlib.suppress(FileExistsError):
+        os.mknod(devpath, mode=0o660 | stat.S_IFBLK, device=os.makedev(major, minor))
+        os.chmod(devpath, 0o660)
+
+
+def _symlink_by_uuid_from_blkid(device_name):
+    devpath = f'/dev/{device_name}'
+    blkid = subprocess.run(
+        ['blkid', '-s', 'UUID', '-o', 'value', devpath],
+        stdout=subprocess.PIPE, check=False,
+    )
+    uuid = blkid.stdout.decode('ascii').strip()
+    if blkid.returncode != 0 or not uuid:
+        return
+
+    by_uuid_dir = pathlib.Path('/dev/disk/by-uuid')
+    by_uuid_dir.mkdir(parents=True, exist_ok=True)
+    with contextlib.suppress(FileExistsError):
+        (by_uuid_dir / uuid).symlink_to(devpath)
 
 
 def _wait_on_udev_for_device_and_partitions(device):
@@ -21,14 +57,22 @@ def _wait_on_udev_for_device_and_partitions(device):
         # However udev processing triggers a rescan of the partitions, removing
         # the entries for a short time. Prevent udev from doing so while we iterate.
         fcntl.flock(f, fcntl.LOCK_EX)
-        partitions = [
-            '/dev/' + entry.name
+        partition_names = [
+            entry.name
             for entry in pathlib.Path('/sys/class/block', device_name).iterdir()
             if entry.name.startswith(device_name)
         ]
 
+    if not _udev_available():
+        for name in (device_name, *partition_names):
+            _mknod_from_sysfs(name)
+            _symlink_by_uuid_from_blkid(name)
+        return
+
     # All partitions need to be mentioned explicitly.
-    subprocess.check_call(['udevadm', 'wait', device, *partitions])
+    partitions = ['/dev/' + name for name in partition_names]
+    subprocess.run(['udevadm', 'wait', device, *partitions],
+                   check=True, timeout=_UDEV_WAIT_TIMEOUT)
 
 
 @contextlib.contextmanager
